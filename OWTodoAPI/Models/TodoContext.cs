71ce3a99f10a2a84277921855ae7702cdcab104a
Oliver Wood <oliver.wood@bitetos.com>
2018-03-01 16:14:46 +0000

    Initial commit of simple project to demonstrate building a .net core web api

---
+++ OWTodoAPI/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace OWTodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
